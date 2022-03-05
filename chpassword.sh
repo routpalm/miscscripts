3c97c2413021c3f16ce8054f5f7f02835126c216
Nicholas A <34774205+routpalm@users.noreply.github.com>
2022-03-05 01:05:07 -0500

    Add files via upload

---
+++ chpassword.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# users=("sylvia" "bennyg" "eleanor" "roger" "karen" "joyce" "raymond" "howard" "suzanne" "louise" "glenn" "christopher" "lynn" "enzo" "peggy" "margaret" "melvin" "wendell")
+users=(sylvia bennyg eleanor roger karen joyce raymond howard suzanne louise glenn christopher lynn enzo peggy margaret melvin wendell)
+for user in ${users[@]}; do
+  echo "Enter password for user ${user}"
+  read password
+  echo -e "${password}\n${password}" | passwd  $user
+done
